565bb5cecb5fb4edbdbe0428e6712651fec40e0c
AmandineBaranger <amande.baranger@gmail.com>
2025-03-25 21:27:42 +0100

    lien entre les trucs

--- Liv-in-paris.Core/Models/DatabaseManager.cs
+++ Liv-in-paris.Core/Models/DatabaseManager.cs
@@ -1,6 +1,36 @@
-﻿namespace Liv_in_paris.Core.Models;
+﻿using System.Data;
+using System.Data.SQLite;
+namespace Liv_in_paris.Core.Models;
 
-public class Test
+public class DatabaseManager
 {
+    private readonly string connectionString;
+
+    public DatabaseManager(string dbPath)
+    {
+        connectionString = $"Data Source={dbPath};Version = 3;";
+    }
     
+
+    public DataTable ExecuteQuery(string query)
+    {
+        using var connection = new SQLiteConnection(connectionString);
+        connection.Open();
+        
+        using var command = new SQLiteCommand(query, connection);
+        using var adapter = new SQLiteDataAdapter(command);
+        
+        var table = new DataTable();
+        adapter.Fill(table);
+        return table;
+    }
+
+    public int ExecuteNonQuery(string query)
+    {
+        using var connection = new SQLiteConnection(connectionString);
+        connection.Open();
+        
+        using var command = new SQLiteCommand(query, connection);
+        return command.ExecuteNonQuery();
+    }
 }
--- Liv-in-paris.Core/Program.cs
+++ Liv-in-paris.Core/Program.cs
@@ -1,5 +1,7 @@
-﻿using Liv_in_paris.Core.Entities;
+﻿using System.Data;
+using Liv_in_paris.Core.Entities;
 using Liv_in_paris.Core.Graph;
+using Liv_in_paris.Core.Models;
 using Liv_in_paris.Core.Services;
 
 namespace Liv_in_paris.Core;
@@ -11,13 +13,28 @@ class Program
     {
         string onglet_1 = "../../../../Files/MetroParis_test_onglet1.csv";
         string onglet_2 = "../../../../Files/MetroParis_test_onglet2.csv";
-        
+
 
         Graphe<Station> graphe = GrapheMetroBuilder.ConstruireDepuisCSV(onglet_1, onglet_2);
 
         Console.WriteLine("Graphe chargé !");
         graphe.AfficherListeAdjacence();
         //graphe.AfficherMatriceAdjacence();
+
+        //Test lien bdd et code
+        var dbPath = "C:\\Users\\chris\\RiderProjects\\Liv-in-Paris\\identifier.sqlite";
+        var db = new DatabaseManager(dbPath);
+        
+        var clients = db.ExecuteQuery("SELECT * FROM Clients");
+
+        foreach (System.Data.DataRow row in clients.Rows )
+        {
+            Console.WriteLine($"{row["Nom"]} - {row["Email"]} - {row["Prenom"]}");
+        }
     }
-    
+
+
+
+
+
 }
