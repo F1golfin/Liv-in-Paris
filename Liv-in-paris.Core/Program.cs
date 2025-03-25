891e234a762f6683e516c7018489ef170cbca8f9
Guillaume <guillaumeblain15@gmail.com>
2025-03-25 21:57:36 +0100

    fichier excel

--- Liv-in-paris.Core/Program.cs
+++ Liv-in-paris.Core/Program.cs
@@ -9,8 +9,8 @@ class Program
     // Ici est éxécuté uniquement le projet .Core
     static void Main(string[] args)
     {
-        string onglet_1 = "../../../../Files/MetroParis_test_onglet1.csv";
-        string onglet_2 = "../../../../Files/MetroParis_test_onglet2.csv";
+        string onglet_1 = "../../../../Files/MetroParis_onglet1.csv";
+        string onglet_2 = "../../../../Files/MetroParis_onglet2.csv";
         
 
         Graphe<Station> graphe = GrapheMetroBuilder.ConstruireDepuisCSV(onglet_1, onglet_2);
