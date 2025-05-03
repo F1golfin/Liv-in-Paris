a9e81e29781cc23fce0c70bb0dbb08f4a5d97872
Guillaume <guillaumeblain15@gmail.com>
2025-05-03 13:39:29 +0200

    Correction de bug

--- Liv-in-paris/Utils/AllLivreeVisibilityConverter.cs
+++ Liv-in-paris/Utils/AllLivreeVisibilityConverter.cs
@@ -6,23 +6,25 @@ using System.Windows;
 using System.Windows.Data;
 using Liv_in_paris.Core.Models;
 
-namespace Liv_in_paris;
-
-public class AllLivreeVisibilityConverter : IValueConverter
+namespace Liv_in_paris
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public class AllLivreeVisibilityConverter : IValueConverter
     {
-        if (value is IEnumerable lignes)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var allLivree = lignes.Cast<LigneCommande>().All(l => l.Statut == "Livree");
-            return allLivree ? Visibility.Visible : Visibility.Collapsed;
+            if (value is IEnumerable lignes)
+            {
+                var allLivree = lignes.Cast<LigneCommande>().All(l => l.Statut == "Livree");
+                return allLivree ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
         }
 
-        return Visibility.Collapsed;
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
